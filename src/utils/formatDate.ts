ad41473b956bebc0b2e0ab2d41a6cad3ed8814da
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2024-02-05 14:22:06 -0300

    feat: add file formatDate with dates

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,3 +1,10 @@
-export function formatDate() {
-  
+export default function formatDate(date: string, times: string) {
+  const data = date;
+  const hora = times;
+
+  const formatData = data.split('-').reverse().join('/');
+
+  const dateAppointment = `${formatData}-${hora}`
+
+  return dateAppointment;
 }
