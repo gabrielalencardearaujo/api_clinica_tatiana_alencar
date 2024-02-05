00bf22886f8f02539640b2c4c42f743bba99517d
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2024-02-05 10:40:36 -0300

    feat: add formatJsonResponses and your funcionality

---
+++ src/utils/formatJsonResponses.ts
@@ -0,0 +1,53 @@
+export default function (statusCode: number, data?: unknown) {
+
+  if (!data) {
+    data = [];
+  };
+
+  switch (statusCode) {
+    case 200:
+
+      return {
+        info: 'Success in Request!',
+        statusCode,
+        data,
+      }
+
+    case 400:
+
+      return {
+        info: 'Invalid Data!',
+        statusCode,
+        data,
+      }
+
+    case 404:
+
+      return {
+        info: 'Not exist!',
+        statusCode,
+        data,
+      }
+
+    case 403:
+
+      return {
+        info: 'Not authorized!',
+        statusCode,
+        data,
+      }
+
+    case 500:
+      return {
+        info: 'An unexpected error occurred in our services, please try later!',
+        statusCode,
+        data,
+      }
+
+    default:
+      return {
+        info: 'Invalid Status Code!',
+        statusCode,
+      }
+  }
+}
